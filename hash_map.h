c08628e5df82ce3d3abb15c883127befee0f0216
Dmitriy Shepelin <shepelin.d@ya.ru>
2020-03-08 14:00:55 +0300

    Problem fixed
    Tests check: https://official.contest.yandex.ru/contest/17022/run-report/31367830/

--- hash_map.h
+++ hash_map.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <list>
@@ -25,74 +26,34 @@ template<
     size_type elements_stored_;
     size_type slots_available_;
 
-    void rehash() {
-      slots_available_ *= 2;
-      table_ = table_type(slots_available_);
-
-      for (iterator iterator_to_key_and_value = list_of_pairs_.begin();
-        iterator_to_key_and_value != list_of_pairs_.end();
-        iterator_to_key_and_value++) {
-        size_type key_hash = hasher_(iterator_to_key_and_value->first);
-        table_[key_hash % slots_available_].push_back(
-          iterator_to_key_and_value);
-      }
-    }
-
   public:
     using iterator = typename list_type::iterator;
     using const_iterator = typename list_type::const_iterator;
 
-    explicit HashMap(Hash hasher = Hash()) : hasher_(hasher),
-      slots_available_(kMinSize), elements_stored_(0),
-      list_of_pairs_(list_type(0)) {
-      table_ = table_type(slots_available_);
-    }
-
-    template<class ForwardIt>
-    HashMap(ForwardIt first, ForwardIt last, Hash hasher = Hash()) :
-      HashMap(hasher) {
-      while (first != last) {
-        insert(*first);
-        first++;
-      }
-    }
-
-    HashMap(std::initializer_list<std::pair<KeyType, ValueType>> pairs_list,
-      Hash hasher = Hash()) :
-      HashMap(pairs_list.begin(), pairs_list.end(), hasher) {}
-
-    iterator find(KeyType key) {
-      size_type key_hash = hasher_(key);
-      for (auto iterator_to_key_and_value : table_[key_hash % slots_available_]) {
-        if (iterator_to_key_and_value != list_of_pairs_.end() &&
-          iterator_to_key_and_value->first == key) {
-          return iterator_to_key_and_value;
-        }
-      }
-
+    iterator end() {
       return list_of_pairs_.end();
     }
 
-    const_iterator find(KeyType key) const {
-      size_type key_hash = hasher_(key);
-      for (auto list_iterator : table_[key_hash % slots_available_]) {
-        if (list_iterator != list_of_pairs_.end() &&
-          list_iterator->first == key) {
-          return list_iterator;
-        }
-      }
-
+    const_iterator end() const {
       return list_of_pairs_.end();
+    }
+
+    iterator begin() {
+      return list_of_pairs_.begin();
+    }
+
+    const_iterator begin() const {
+      return list_of_pairs_.begin();
     }
 
     iterator insert(std::pair<KeyType, ValueType> key_and_value) {
       iterator found_pair_iterator = find(key_and_value.first);
-      if (found_pair_iterator != list_of_pairs_.end()) {
+      if (found_pair_iterator != end()) {
         return found_pair_iterator;
       }
 
       list_of_pairs_.push_front(key_and_value);
-      iterator list_iterator = list_of_pairs_.begin();
+      iterator list_iterator = begin();
       size_type key_hash = hasher_(key_and_value.first);
 
       table_[key_hash % slots_available_].push_back(list_iterator);
@@ -105,10 +66,59 @@ template<
       return list_iterator;
     }
 
+  private:
+    void rehash() {
+      slots_available_ *= 2;
+      table_ = table_type(slots_available_);
+
+      for (iterator iterator_to_key_and_value = begin();
+        iterator_to_key_and_value != end();
+        iterator_to_key_and_value++) {
+        size_type key_hash = hasher_(iterator_to_key_and_value->first);
+        table_[key_hash % slots_available_].push_back(
+          iterator_to_key_and_value);
+      }
+    }
+
+  public:
+    explicit HashMap(Hash hasher = Hash()) : hasher_(hasher),
+      list_of_pairs_(list_type(0)), elements_stored_(0),
+      slots_available_(kMinSize) {
+      table_ = table_type(slots_available_);
+    }
+
+    template<class ForwardIt>
+    HashMap(ForwardIt first, ForwardIt last, Hash hasher = Hash()) :
+      HashMap(hasher) {
+      for (; first != last; first++) {
+        insert(*first);
+      }
+    }
+
+    HashMap(std::initializer_list<std::pair<KeyType, ValueType>> pairs_list,
+      Hash hasher = Hash()) :
+      HashMap(pairs_list.begin(), pairs_list.end(), hasher) {}
+
+    iterator find(KeyType key) {
+      size_type key_hash = hasher_(key);
+      for (auto iterator_to_key_and_value : table_[key_hash % slots_available_]) {
+        if (iterator_to_key_and_value != end() &&
+          iterator_to_key_and_value->first == key) {
+          return iterator_to_key_and_value;
+        }
+      }
+
+      return list_of_pairs_.end();
+    }
+
+    const_iterator find(KeyType key) const {
+      return const_cast<HashMap*>(this)->find(key);
+    }
+
     void erase(KeyType key) {
       iterator list_iterator = find(key);
 
-      if (list_iterator == list_of_pairs_.end()) {
+      if (list_iterator == end()) {
         return;
       }
 
@@ -117,32 +127,18 @@ template<
 
       auto& list_of_iters = table_[key_hash % slots_available_];
       for (auto table_list_it = list_of_iters.begin();
-        table_list_it != list_of_iters.end(); table_list_it++)
+        table_list_it != list_of_iters.end();
+        table_list_it++) {
         if ((*table_list_it)->first == key) {
           list_of_iters.erase(table_list_it);
           list_of_pairs_.erase(list_iterator);
           return;
         }
+      }
     }
 
     size_type size() const {
       return elements_stored_;
-    }
-
-    iterator begin() {
-      return list_of_pairs_.begin();
-    }
-
-    const_iterator begin() const {
-      return list_of_pairs_.begin();
-    }
-
-    iterator end() {
-      return list_of_pairs_.end();
-    }
-
-    const_iterator end() const {
-      return list_of_pairs_.end();
     }
 
     bool empty() const {
@@ -161,8 +157,9 @@ template<
 
     const ValueType& at(KeyType key) const {
       const_iterator list_iterator = find(key);
-      if (list_iterator == list_of_pairs_.end())
+      if (list_iterator == end()) {
         throw std::out_of_range("");
+      }
 
       return list_iterator->second;
     }
@@ -176,8 +173,9 @@ template<
 
     HashMap& operator= (HashMap hash_map) {
       clear();
-      for (auto key_and_value : hash_map)
+      for (auto key_and_value : hash_map) {
         insert(key_and_value);
+      }
       return *this;
     }
 };
